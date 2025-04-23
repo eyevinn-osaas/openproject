cd79374d9220f936b2df14d4619d2e62ae5186f1
Jonas Birmé <jonas.birme@eyevinn.se>
2025-04-23 17:07:09 +0200

    fix: seed database with defaults

--- osc-entrypoint.sh
+++ osc-entrypoint.sh
@@ -8,5 +8,10 @@ else
   echo "No OpenProject hostname provided, using default."
   export OPENPROJECT_HTTPS=false
 fi
+export OPENPROJECT_SECRET_KEY_BASE=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo ''`
+export OPENPROJECT_SEED_ADMIN_USER_PASSWORD=admin
+export OPENPROJECT_SEED_ADMIN_USER_PASSWORD_RESET=true
+
 ./bin/rails db:migrate RAILS_ENV=production
+./bin/rails db:seed RAILS_ENV=production
 /app/docker/prod/entrypoint-slim.sh "$@"
